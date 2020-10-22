be13670ba1cfa0ced633cdaa24ea0757ce19cc46
MiMNets <mdmonirulislammonir@gmail.com>
2020-10-22 09:38:33 +0600

    logo and navbar

---
+++ src/components/Home/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from '../NavBar/NavBar';
+
+const Header = () => {
+    return (
+        <div>
+            <NavBar></NavBar>
+            <h1>This is header</h1>
+        </div>
+    );
+};
+
+export default Header;
